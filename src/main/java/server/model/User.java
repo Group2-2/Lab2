0cbad71c3992be70775fdbe68928af2328dedaa5
Anton Kudryavtsev <kam123ua@gmail.com>
2018-03-01 22:17:20 +0200

    Update toString user

--- src/main/java/server/model/User.java
+++ src/main/java/server/model/User.java
@@ -69,6 +69,8 @@ public class User {
                 "name='" + name + '\'' +
                 ", login='" + login + '\'' +
                 ", password='" + password + '\'' +
+                ", online=" + online +
+                ", admin=" + admin +
                 '}';
     }
 
