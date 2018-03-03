5c02d5870af35843749db2756f27e184c7920da6
Oljk <oljk2512@gmail.com>
2018-03-03 22:13:46 +0200

    оприем-ответ клиенту

--- src/main/java/server/Main.java
+++ src/main/java/server/Main.java
@@ -1,42 +1,12 @@
 package server;
 
-import com.thoughtworks.xstream.XStream;
-import server.model.FilePath;
-import server.model.Message;
-import server.model.User;
-import server.model.XmlConfiguration;
+import server.controller.Server;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
-import javax.xml.bind.PropertyException;
-import java.io.StringWriter;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        XmlConfiguration configuration = XmlConfiguration.getInstance();
-
-        System.out.println(configuration.configuration("<command type=\"logi6n\" result = \"ACCEPTED\" isAdmin = \"false\"/>"));
-
-        /*XStream xstream = new XStream();
-        List<Message> messageList = new ArrayList<>();
-        String buf = XmlConfiguration.getInstance().configuration("<command type=\"addMessage\" sender=\"my_nick\" chat_id = \"0\" text =\"sdfgdsf\"/>");
-        System.out.println(buf);
-
-        //    messageList.add(new Message(user, "1"));
-      //  messageList.add(new Message(user, "2"));
-        System.out.println(xstream.toXML(messageList));
-        System.out.println(xstream.fromXML(xstream.toXML(messageList)));
-        /*JAXBContext jaxbContext = JAXBContext.newInstance(User.class);
-        Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
-        jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-        StringWriter sw = new StringWriter();
-
-        jaxbMarshaller.marshal(user, sw);
-        String xmlString = sw.toString();
-        System.out.println(xmlString);*/
+       Server server = Server.getInstance();
+       new Thread(server).start();
     }
 }
--- src/main/java/server/controller/Connection.java
+++ src/main/java/server/controller/Connection.java
@@ -1,14 +1,16 @@
 package server.controller;
 
-import server.model.User;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import server.model.*;
 import java.io.*;
 import java.net.*;
 
 public class Connection implements Runnable {
 
-    private Socket socket;
+    final private Socket socket;
     private PrintWriter writer;
-    private User user = null;
     private boolean isWork = true;
 
     public Connection(Socket socket) {
@@ -18,16 +20,29 @@ public class Connection implements Runnable {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        new Thread(this::checkOnline).start();
     }
 
     public void run() {
         while (isWork) {
             try {
                 BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                String message = reader.readLine();
+                if (message != null && !message.equals("")) {
+                    String response = XmlConfiguration.getInstance().configuration(message);
+                    String login = checkNewUser(message);
+                    if(!login.equals("")){
+                        Server.getInstance().setUser(login, this);
+                    }
+                    send(response);
+                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
+        }
+        try {
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
     }
@@ -36,15 +51,32 @@ public class Connection implements Runnable {
         writer.println(message);
     }
 
-    private void checkOnline() {
-        while (isWork) {
+    public boolean checkConnection() {
             try {
                 send("<test></test>");
+                return true;
             } catch (Exception e) {
-                if (user != null)
-                    Server.getInstance().getUsers().remove(user);
                 isWork = false;
+               return false;
             }
+    }
+    private String checkNewUser(String command){
+        Document document = XmlConfiguration.newDocument(command);
+        NodeList nodes = document.getElementsByTagName("command");
+        Element element = (Element) nodes.item(0);
+        String type = element.getAttribute("type");
+        switch (type) {
+            case "registration":
+            case "login" :
+                return element.getAttribute("login");
+            case "addMessage":
+            case "newChatID":
+            case "addToChat":
+                long id = Long.parseLong(element.getAttribute("chat_id"));
+                Server.getInstance().sendToChat(id,command, this);
+                return "";
+            default :
+                return "";
         }
     }
 }
